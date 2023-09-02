e71666cfb8da020f66d9fc2f10f62e2f4fb4a35c
Sau64 <sauron@gxzone.com>
2023-09-01 21:37:03 -0300

    Update NestedValidationException.php

--- library/Exceptions/NestedValidationException.php
+++ library/Exceptions/NestedValidationException.php
@@ -150,7 +150,7 @@ class NestedValidationException extends ValidationException implements IteratorA
     /**
      * @return SplObjectStorage
      */
-    #[ReturnTypeWillChange]
+    #[\ReturnTypeWillChange]
     public function getIterator()
     {
         $childrenExceptions = new SplObjectStorage();
